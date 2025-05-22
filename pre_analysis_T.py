89c74a285a857a12b5eb51a2d5fde4f98025032d
hainingpan <hnpan@terpmail.umd.edu>
2025-05-22 10:56:42 -0500

    update pre_analysis_T to allow remaining string saved

--- pre_analysis_T.py
+++ pre_analysis_T.py
@@ -38,7 +38,7 @@ for fixed_params,vary_params in params_list:
         # fn_template='MPS_({nu},{de})_L{L}_pctrl{p_ctrl:.3f}_pproj{p_proj:.3f}_sC{sC}_sm{sm}_x01_shots_bitstring_T.json',
         # fn_template='MPS_({nu},{de})_L{L}_pctrl{p_ctrl:.3f}_pproj{p_proj:.3f}_sC{sC}_sm{sm}_x01_evo.json',
         # fn_dir_template='./MPS_0-1_C_m_x00',
-        fn_dir_template=f'../CT_MPS/MPS_0-1_C_m_x01_T_L{L}',
+        fn_dir_template=f'/p/work/hpan/CT_MPS/MPS_0-1_C_m_x01_T_L{L}',
         # fn_dir_template=f'../CT_MPS/MPS_0-1_C_m_O_T_L{L}',
         # fn_dir_template=f'../CT_MPS/MPS_0-1_shots_T_L{L}',
         # fn_dir_template=f'../CT_MPS/MPS_0-1_shots_bitstring_T_L{L}',
@@ -56,7 +56,7 @@ for fixed_params,vary_params in params_list:
 
 with open(f'params_CT_MPS_0_C_m_T_L{L}_series.txt','r') as f:
     # lines=f.readlines()
-    linewidth=7500
+    linewidth=250000
     count=0
     total_string = []
     string = ''
@@ -67,6 +67,9 @@ with open(f'params_CT_MPS_0_C_m_T_L{L}_series.txt','r') as f:
             count=0
             total_string.append(string[1:])
             string= ''
+    # Add the remaining part if it exists
+    if string:
+        total_string.append(string[1:])
 
 with open(f'params_CT_MPS_0_C_m_T_L{L}_series.txt','w') as f:
     f.write('\n'.join(total_string))
--- submit_multiproc.sh
+++ submit_multiproc.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #PBS -A ONRDC54450755
-#PBS -l walltime=2:00:00
-#PBS -q standard_sm
+#PBS -l walltime=0:59:00
+#PBS -q debug
 #PBS -l select=1:ncpus=192:mpiprocs=1
-#PBS -N L40
+#PBS -N L12
 #PBS -m abe
 #PBS -M hnpanboa@gmail.com
 #PBS -r y
-#PBS -J 1-18
-cd $WORKDIR/CT_MPS
+#PBS -J 1-3
+# cd $WORKDIR/CT_MPS
+cd $HOME/CT_MPS
 
 # how many processes?
 NP=192
@@ -17,12 +18,15 @@ NP=192
 export JULIA_NUM_THREADS=2
 export OPENBLAS_NUM_THREADS=$JULIA_NUM_THREADS
 
-PARAMS_FILE="params_CT_MPS_0_C_m_T_L40_series.txt"
+PARAMS_FILE="params_CT_MPS_0_C_m_T_L12_series.txt"
 params=$(sed -n "${PBS_ARRAY_INDEX}p" "$PARAMS_FILE")
+echo -n "$params" | wc -c
 
 export JULIA_DEPOT_PATH=~/julia_depot
+export APPTAINER_BIND="/p/work/hpan"
 
-singularity exec $WORKDIR/julia_itensors.sif \
+
+singularity exec $HOME/julia_itensors_improved.sif \
   julia \
     --sysimage ct_with_wrapper.so \
     --project=./CT \
