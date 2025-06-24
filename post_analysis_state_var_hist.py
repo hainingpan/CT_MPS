2f03d2589b77bd5257c104a061338e0d4974ca6f
hainingpan <hnpan@terpmail.umd.edu>
2025-06-24 17:05:39 -0500

    add post-analysis files

--- post_analysis_state_var_hist.py
+++ post_analysis_state_var_hist.py
@@ -1,29 +1,34 @@
-# This script is compute the sum of trajectory fluctuation and the "trajectory-averaged" state fluctuation, given a specific circuit.
+# This script is used to compute histogram true state variance as in the paper, 
+# the clear distinction is that , it does not take the "trajector-average" of state fluctuation, which is consistent with the paper. Also to save the disk space, it automatically take out the "steady state" from L^2 to 2L^2
+
 import sys
 dir_path='../control_transition'
 sys.path.append(dir_path)
+# import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 from plot_utils import *
+
 L=16
 params_list=[
 ({'nu':0,'de':1,},
 {
 # 'p_ctrl':[.47,.49,.51,.53],
-'p_ctrl':[.4,.5,.6],
+'p_ctrl':[0.4,0.5,.6],
 # 'p_ctrl':[0.4,0.45,0.47,0.49,0.5,0.51,0.53,0.55,0.6],
-# 'p_ctrl':[.65,.7,.75,.8,.85],
-# 'p_ctrl':[0.1],
 'p_proj':np.linspace(0.0,0.0,1),
 'sC':np.arange(0,500),
 'sm':np.arange(500),
-'L':[L]
+'L':[L,]
 }
 ),
 ]
+# ob="DW"
 ob="O"
 ob1=ob+'1'
 ob2=ob+'2'
+
+err=1e-5
 for fixed_params,vary_params in params_list:
     data_MPS_0_T_DW_dict=generate_params(
         fixed_params=fixed_params,
@@ -41,36 +46,32 @@ for fixed_params,vary_params in params_list:
         # zip_fn='./MPS_0-1_C_m_x01_T.zip'  
         # zip_fn=f'./MPS_0-1_C_m_x01_T_L{L}.zip'  
         zip_fn=f'./MPS_0-1_C_m_O_T_L{L}.zip'  
-        # zip_fn=f'./MPS_0-1_shots_L{L}.zip' 
+        # zip_fn=f'./MPS_0-1_MIPT_T_L{L}.zip'  
     )
 df_MPS_0_T_DW=convert_pd(data_MPS_0_T_DW_dict,names=['Metrics','sm','sC','p_ctrl','L','p_proj',])
 
-def traj_qvar(df,L,p_ctrl,sC):
-    # trajecotry fluctuation, fix SC, as a function T 
-    data = df['observations'].xs(sC,level='sC').xs(p_ctrl,level='p_ctrl').xs(0.0,level='p_proj').xs(L,level='L')
-    data_DW1=np.stack(data.xs(ob1,level='Metrics'))
-    sigma_mc=data_DW1.var(axis=0)
+def state_var(df, L,p_ctrl,sC,p_proj=0):
+    data=df['observations'].xs(sC,level='sC').xs(p_ctrl,level='p_ctrl').xs(L,level='L').xs(p_proj,level='p_proj')
+    data_ob1=np.stack(data.xs(ob1,level='Metrics'))
+    data_ob2=np.stack(data.xs(ob2,level='Metrics'))
+    return (data_ob2-data_ob1**2)
 
-    # state_fluctuation
-    data_DW2=np.stack(data.xs(ob2,level='Metrics'))
-    qvar=(data_DW2-data_DW1**2)
-    sigma_s=qvar.mean(axis=0)
-
-    return sigma_mc+sigma_s
-
-sigma_shot={}
+    
+state_var_dict={}
 for p in tqdm(params_list[0][1]['p_ctrl']):
     for L in params_list[0][1]['L']:
         print(p,L)
-        sigma_shot[(p,L)]=[]
+        state_var_list = []
         for sC in range((params_list[0][1]['sC']).shape[0]):
-            # try:
-            sigma_shot_=traj_qvar(df_MPS_0_T_DW,L=L,p_ctrl=p,sC=sC)
-            sigma_shot[(p,L)].append(sigma_shot_)
-            # except:
-            #     pass
-        sigma_shot[(p,L)]=np.array(sigma_shot[(p,L)])
+            try:
+                state_var_=state_var(df_MPS_0_T_DW,L=L,p_ctrl=p,sC=sC)
+                state_var_list.append(state_var_)
+            except Exception as e:
+                print(f"Error at p_ctrl={p}, L={L}, sC={sC}: {e}")
+        state_var_dict[p,L]=np.array(state_var_list)
+        
+# with open(f'state_var_hist_C_m_T_L{L}.pickle','wb') as f:
+# with open(f'state_var_hist_C_m_T_O_L{L}.pickle','wb') as f:
+with open(f'state_var_hist_{ob}_L{L}.pickle','wb') as f:
 
-# with open(f'traj_state_sum_DW_L{L}.pickle','wb') as f:
-with open(f'traj_state_sum_{ob}_L{L}.pickle','wb') as f:
-    pickle.dump(sigma_shot,f)
+    pickle.dump(state_var_dict,f)
--- post_analysis_traj_var.py
+++ post_analysis_traj_var.py
@@ -5,32 +5,32 @@ sys.path.append(dir_path)
 
 from tqdm import tqdm
 from plot_utils import *
-L=20
+L=10
 params_list=[
 ({'nu':0,'de':1,},
 {
 # 'p_ctrl':[.47,.49,.51,.53],
-# 'p_ctrl':[.4,.5,.6],
+'p_ctrl':[.4,.5,.6],
 # 'p_ctrl':[0.4,0.45,0.47,0.49,0.5,0.51,0.53,0.55,0.6],
 # 'p_ctrl':[0.1],
-'p_ctrl':[0.],
-# 'p_proj':np.linspace(0.0,0.0,1),
-'p_proj':np.round(np.arange(0.4,1,0.1),2),
+# 'p_ctrl':[0.],
+'p_proj':np.linspace(0.0,0.0,1),
+# 'p_proj':np.round(np.arange(0.4,1,0.1),2),
 'sC':np.arange(0,500),
 'sm':np.arange(500),
 'L':[L]
 }
 ),
 ]
-ob="DW"
+ob="O"
 ob1=ob+'1'
 ob2=ob+'2'
 for fixed_params,vary_params in params_list:
     data_MPS_0_T_DW_dict=generate_params(
         fixed_params=fixed_params,
         vary_params=vary_params,
-        fn_template='MPS_({nu},{de})_L{L}_pctrl{p_ctrl:.3f}_pproj{p_proj:.3f}_sC{sC}_sm{sm}_x01_DW_T.json',
-        # fn_template='MPS_({nu},{de})_L{L}_pctrl{p_ctrl:.3f}_pproj{p_proj:.3f}_sC{sC}_sm{sm}_O_T.json',
+        # fn_template='MPS_({nu},{de})_L{L}_pctrl{p_ctrl:.3f}_pproj{p_proj:.3f}_sC{sC}_sm{sm}_x01_DW_T.json',
+        fn_template='MPS_({nu},{de})_L{L}_pctrl{p_ctrl:.3f}_pproj{p_proj:.3f}_sC{sC}_sm{sm}_O_T.json',
         fn_dir_template='/MPS_0-1_C_m_x01_T',
         input_params_template='',
         load_data=load_zip_json,
@@ -42,11 +42,11 @@ for fixed_params,vary_params in params_list:
         # zip_fn='./MPS_0-1_C_m_x01_T.zip'  
         # zip_fn=f'./MPS_0-1_C_m_x01_T_L{L}.zip'
         # zip_fn=f'/home/jake/Data/MPS_0-1_C_m_x01_T_L{L}.zip'  
-        # zip_fn=f'./MPS_0-1_C_m_O_T_L{L}.zip'  
+        zip_fn=f'./MPS_0-1_C_m_O_T_L{L}.zip'  
         # zip_fn=f'./MPS_0-1_shots_L{L}.zip' 
-        zip_fn=f'./MPS_0-1_MIPT_T_L{L}.zip' 
+        # zip_fn=f'./MPS_0-1_MIPT_T_L{L}.zip' 
     )
-df_MPS_0_T_DW=convert_pd(data_MPS_0_T_DW_dict,names=['Metrics','sm','sC','p_ctrl','L','p_proj',])
+df_MPS_0_T=convert_pd(data_MPS_0_T_DW_dict,names=['Metrics','sm','sC','p_ctrl','L','p_proj',])
 
 # def trajvar(df,L,p_ctrl,sC):
 #     data=df.xs(L,level='L').xs(p_ctrl,level='p_ctrl').xs(0.0,level='p_proj').xs(sC,level='sC')
@@ -62,25 +62,26 @@ def trajvar(df,L,p_ctrl,sC,p_proj=0):
     sigma_mc=data_DW1.var(axis=0)
     return sigma_mc
 
-traj_var_dw_dict={}
-traj_var_dw_sem_dict={}
-sC_traj_var_dw={}
-# for p in tqdm(params_list[0][1]['p_ctrl']):
-for p in tqdm(params_list[0][1]['p_proj']):
+traj_var_dict={}
+traj_var_sem_dict={}
+sC_traj_var={}
+for p in tqdm(params_list[0][1]['p_ctrl']):
+# for p in tqdm(params_list[0][1]['p_proj']):
     for L in params_list[0][1]['L']:
         print(p,L)
-        # sC_traj_var_dw=[trajvar(df_MPS_0_T_DW,L=L,p_ctrl=p,sC=sC) for sC in range((params_list[0][1]['sC']).shape[0])]
-        sC_traj_var_dw[(p,L)]=[]
+        # sC_traj_var=[trajvar(df_MPS_0_T,L=L,p_ctrl=p,sC=sC) for sC in range((params_list[0][1]['sC']).shape[0])]
+        sC_traj_var[(p,L)]=[]
         for sC in range((params_list[0][1]['sC']).shape[0]):
             try:
-                sC_traj_var_dw[(p,L)].append(trajvar(df_MPS_0_T_DW,L=L,p_ctrl=0,sC=sC,p_proj=p))
+                sC_traj_var[(p,L)].append(trajvar(df_MPS_0_T,L=L,p_ctrl=p,sC=sC,p_proj=0))
             except:
                 pass
-        sC_traj_var_dw[(p,L)]=np.array(sC_traj_var_dw[(p,L)])
-        traj_var_dw_dict[(p,L)], traj_var_dw_sem_dict[(p,L)]=np.mean(sC_traj_var_dw[(p,L)],axis=0), np.std(sC_traj_var_dw[(p,L)],axis=0)/np.sqrt((params_list[0][1]['sC']).shape[0])
+        sC_traj_var[(p,L)]=np.array(sC_traj_var[(p,L)])
+        traj_var_dict[(p,L)], traj_var_sem_dict[(p,L)]=np.mean(sC_traj_var[(p,L)],axis=0), np.std(sC_traj_var[(p,L)],axis=0)/np.sqrt((params_list[0][1]['sC']).shape[0])
 
 # with open(f'C_m_T_L{L}.pickle','wb') as f:
 # with open(f'traj_var_C_m_T_O_L{L}.pickle','wb') as f:
 # with open(f'circ_var_shots_L{L}.pickle','wb') as f:
-with open(f'traj_var_C_m_T_MIPT_L{L}.pickle','wb') as f:
-    pickle.dump([sC_traj_var_dw,traj_var_dw_dict,traj_var_dw_sem_dict],f)
+# with open(f'traj_var_C_m_T_MIPT_L{L}.pickle','wb') as f:
+with open(f'traj_var_{ob}_L{L}.pickle','wb') as f:
+    pickle.dump([sC_traj_var,traj_var_dict,traj_var_sem_dict],f)
