766d4c56054789eb3eada1b8b4fd0c8a129ab2b3
hainingpan <hnpan@terpmail.umd.edu>
2025-07-14 20:46:12 -0500

    Update postprocess_XEB.jl

--- postprocess_XEB.jl
+++ postprocess_XEB.jl
@@ -6,25 +6,34 @@ using Serialization
 using ProgressMeter
 using Printf
 using ITensors
+using JSON
 
 directory = "/p/work/hpan/CT_MPS/"
 L=20
-pctrl_list = [.2,.25,.3,.35,.4,.45,.5,.55,.6,.65,.7,.75,.8,.85]
-sC_list = 1:50
+# pctrl_list = [.2,.25,.3,.35,.4,.45,.5,.55,.6,.65,.7,.75,.8,.85]
+# sC_list = 1:50
+pctrl_list = [.85]
+sC_list = 1:9
 
 
 function calc_(L,pctrl,sC;sm_max=9)
     data_abs2_list = MPS[];
     for sm in 0:sm_max
         filename = @sprintf("MPS_(0,1)_L%d_pctrl%.3f_sC%d_sm%d_XEB.jls", L, pctrl, sC, sm)
+        # @printf("Processing file: %s\n", filename)
         if haskey(file_index, filename)
             data = deserialize(z.files[(file_index[filename])]);
             data_abs2 = CT.elementwise_product(data.mps, conj(data.mps); cutoff=1e-5);
             push!(data_abs2_list, data_abs2);
         end
     end
-    data_abs2_mean_list = CT.sum_mps_tree(data_abs2_list)/length(data_abs2_list);
-    return abs(inner(data_abs2_mean_list ,conj(data_abs2_mean_list)))
+    if isempty(data_abs2_list)
+        println("No data found for pctrl=$(pctrl), sC=$(sC)")
+        return NaN
+    else
+        data_abs2_mean_list = CT.sum_mps_tree(data_abs2_list)/length(data_abs2_list)
+        return abs(inner(data_abs2_mean_list ,conj(data_abs2_mean_list)))
+    end
 end
 
 function create_zip_index(zip_reader)
@@ -90,9 +99,10 @@ file_index = create_zip_index(z);
 
 results = Matrix{Float64}(undef, length(pctrl_list), length(sC_list))
 @showprogress for (i,pctrl) in enumerate(pctrl_list), (j,sC) in enumerate(sC_list)
+    # @printf("Processing pctrl=%.3f, sC=%d\n", pctrl, sC)
     results[i,j] = calc_(L, pctrl, sC)
 end
-using JSON
+
 open("L$(L)_XEB_sim.json", "w") do io
     write(io, JSON.json(Dict("results"=>results, "pctrl_list"=>pctrl_list, "sC_list"=>sC_list)))
 end
