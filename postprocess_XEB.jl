5a7da410a9584c780868d842101967712f7817fb
hainingpan <hnpan@terpmail.umd.edu>
2025-07-17 22:50:52 -0500

    update postprocess_XEB.jl

--- postprocess_XEB.jl
+++ postprocess_XEB.jl
@@ -7,24 +7,46 @@ using ProgressMeter
 using Printf
 using ITensors
 using JSON
+using ArgParse
+using Random
 
-directory = "/p/work/hpan/CT_MPS/"
-L=20
-# pctrl_list = [.2,.25,.3,.35,.4,.45,.5,.55,.6,.65,.7,.75,.8,.85]
-# sC_list = 1:50
-pctrl_list = [.85]
-sC_list = 1:9
+function parse_commandline()
+    s = ArgParseSettings()
+    
+    @add_arg_table s begin
+        "p_value"
+            help = "Control probability value"
+            arg_type = Float64
+            required = true
+        "L_value"
+            help = "System size L"
+            arg_type = Int
+            required = true
+        "--sample-size"
+            help = "Monte Carlo sample size"
+            arg_type = Int
+            default = 10000
+        "--seed"
+            help = "Random seed"
+            arg_type = Int
+            default = 0
+        "--sm-max"
+            help = "Maximum sm value"
+            arg_type = Int
+            default = 500
+    end
+    
+    return parse_args(s)
+end
 
-
-function calc_(L,pctrl,sC;sm_max=9)
+function calc_(L,pctrl,sC,z,file_index;sm_max=500)
     data_abs2_list = MPS[];
     for sm in 0:sm_max
         filename = @sprintf("MPS_(0,1)_L%d_pctrl%.3f_sC%d_sm%d_XEB.jls", L, pctrl, sC, sm)
-        # @printf("Processing file: %s\n", filename)
         if haskey(file_index, filename)
-            data = deserialize(z.files[(file_index[filename])]);
-            data_abs2 = CT.elementwise_product(data.mps, conj(data.mps); cutoff=1e-5);
-            push!(data_abs2_list, data_abs2);
+            data = deserialize(z.files[(file_index[filename])])
+            data_abs2 = CT.elementwise_product(data.mps, conj(data.mps); cutoff=1e-5)
+            push!(data_abs2_list, data_abs2)
         end
     end
     if isempty(data_abs2_list)
@@ -34,6 +56,33 @@ function calc_(L,pctrl,sC;sm_max=9)
         data_abs2_mean_list = CT.sum_mps_tree(data_abs2_list)/length(data_abs2_list)
         return abs(inner(data_abs2_mean_list ,conj(data_abs2_mean_list)))
     end
+end
+
+function monte_carlo_abs2(L::Int,pctrl::Float64,sC::Int,sm::Int,z,file_index;sample_size::Int=10000, seed::Int=0, counter::Dict{Int, Int}=Dict{Int, Int}())
+    rng = MersenneTwister(seed)
+    filename = @sprintf("MPS_(0,1)_L%d_pctrl%.3f_sC%d_sm%d_XEB.jls", L, pctrl, sC, sm)
+    @printf("Processing file: %s\n", filename)
+    
+    if haskey(file_index, filename)
+        data = deserialize(z.files[(file_index[filename])])
+        for i in 1:sample_size
+            sample_ = sample!(rng, data.mps)
+            rs = CT.bin2dec(sample_.-1)
+            counter[rs] = get(counter, rs, 0) + 1
+        end
+    end
+
+    return counter
+end
+
+function calc_MC(L::Int,pctrl::Float64,sC::Int,z,file_index;sm_max=500, sample_size::Int=10000, seed::Int=0)
+    counter = Dict{Int, Int}()
+    for sm in 0:sm_max-1
+        @printf("Processing sm=%d\n", sm)
+        _ = monte_carlo_abs2(L,pctrl,sC,sm,z,file_index;sample_size=sample_size, seed=seed, counter=counter)
+    end
+    vec = collect(values(counter))
+    return sum((vec/sum(vec)).^2)
 end
 
 function create_zip_index(zip_reader)
@@ -93,18 +142,60 @@ function list_files_in_zip(zip_reader)
     end
 end
 
-path = joinpath(directory, "MPS_0-1_XEB_L$(L).zip")
-z = ZipFile.Reader( path);
-file_index = create_zip_index(z);
-
-results = Matrix{Float64}(undef, length(pctrl_list), length(sC_list))
-params = [(i,j,pctrl,sC) for (i,pctrl) in enumerate(pctrl_list) for (j,sC) in enumerate(sC_list)]
-@showprogress for (i,j,pctrl,sC) in params
-    results[i,j] = calc_(L, pctrl, sC)
+function main()
+    # Parse command line arguments
+    args = parse_commandline()
+    
+    p_value = args["p_value"]
+    L = args["L_value"]
+    sample_size = args["sample-size"]
+    seed = args["seed"]
+    sm_max = args["sm-max"]
+    
+    println("Running with parameters:")
+    println("  p_value: $(p_value)")
+    println("  L: $(L)")
+    println("  sample_size: $(sample_size)")
+    println("  seed: $(seed)")
+    println("  sm_max: $(sm_max)")
+    
+    start_time = time()
+    
+    directory = "/p/work/hpan/CT_MPS/"
+    path = joinpath(directory, "MPS_0-1_XEB_L$(L).zip")
+    z = ZipFile.Reader(path)
+    file_index = create_zip_index(z)
+    
+    # pctrl_list = [.2,.25,.3,.35,.4,.45,.5,.55,.6,.65,.7,.75,.8,.85]
+    # sC_list = 1:50
+    pctrl_list = [p_value]
+    sC_list = 1:49
+    
+    results = Matrix{Float64}(undef, length(pctrl_list), length(sC_list))
+    params = [(i,j,pctrl,sC) for (i,pctrl) in enumerate(pctrl_list) for (j,sC) in enumerate(sC_list)]
+    for (i,j,pctrl,sC) in params
+        # results[i,j] = calc_(L, pctrl, sC, z, file_index)
+        results[i,j] = calc_MC(L, pctrl, sC, z, file_index, sample_size=sample_size, sm_max=sm_max)
+    end
+    
+    open("XEB_q_L$(L)_p$(p_value)_samples$(sample_size)_seed$(seed)_smmax$(sm_max).json", "w") do io
+        write(io, JSON.json(Dict(
+            "results"=>results, 
+            "pctrl_list"=>pctrl_list, 
+            "sC_list"=>sC_list, 
+            "L"=>L, 
+            "p"=>p_value,
+            "sample_size"=>sample_size,
+            "seed"=>seed,
+            "sm_max"=>sm_max
+        )))
+    end
+    
+    execution_time = time() - start_time
+    println(@sprintf("Total execution time: %.2f seconds (%.2f minutes)", execution_time, execution_time/60))
 end
 
-open("L$(L)_XEB_sim.json", "w") do io
-    write(io, JSON.json(Dict("results"=>results, "pctrl_list"=>pctrl_list, "sC_list"=>sC_list)))
+# Run main function if script is executed directly
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
 end
-
-
